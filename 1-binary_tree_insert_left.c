d127591bd15e779de936cd418b11d0efb9cc7a14
Houcine Walaq <houcine.walaq@gmai.com>
2023-11-28 11:30:32 +0100

    1 attempt

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -28,7 +28,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	else
 	{
 		created_node->left = parent->left;
-		created_node->left->parent = created_node;
+		parent->left->parent = created_node;
 		parent->left = created_node;
 	}
 	return (created_node);

6dfab72908f5d531d62729dab9bd99713f69d502
Houcine Walaq <houcine.walaq@gmai.com>
2023-11-28 16:04:49 +0100

    10 b

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,15 +1,22 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_depth - Measure the depth of a node in a binary tree.
+ *
+ * @tree: A pointer to the node to measure the depth.
+ *
+ * Return: The depth of the node in the binary tree.
+ */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-    int depht_count;
-    
-    if (tree == NULL)
-        return (0);
-    
-    if (tree->parent == NULL)
-        return (0);
-    
-    depht_count = binary_tree_depth(tree->parent) + 1;
-    return depht_count;
+	int depht_count;
+
+	if (tree == NULL)
+		return (0);
+
+	if (tree->parent == NULL)
+		return (0);
+
+	depht_count = binary_tree_depth(tree->parent) + 1;
+	return (depht_count);
 }

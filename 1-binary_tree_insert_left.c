81c44c737bf7753dbe12614b7eb0154e8ddc7212
Houcine Walaq <houcine.walaq@gmai.com>
2023-11-28 11:42:36 +0100

    1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -21,7 +21,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 
 	created_node->n = value;
 	created_node->parent = parent;
-	created-node->right = NULL;
+	created_node->left = NULL;
 	created_node->right = NULL;
 
 	if (parent->left == NULL)

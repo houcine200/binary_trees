31a97a936998536af79304a9a2b347a5e44fce50
Houcine Walaq <houcine.walaq@gmai.com>
2023-11-28 15:39:57 +0100

    9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,6 +7,9 @@ size_t binary_tree_height(const binary_tree_t *tree)
     if (tree == NULL)
         return (0);
     
+    if (tree->left == NULL && tree->right == NULL)
+        return (0);
+    
     height_left = binary_tree_height(tree->left) + 1;
     height_right = binary_tree_height(tree->right) + 1;
     

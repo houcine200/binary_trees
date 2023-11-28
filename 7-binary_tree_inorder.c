a33bfcbdc04c56ee4d640538531bf8701ff04de1
Houcine Walaq <houcine.walaq@gmai.com>
2023-11-28 14:41:39 +0100

    betty

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -1,6 +1,6 @@
 #include "binary_trees.h"
 /**
- * binary_tree_preorder - Perform pre-order traversal on a binary tree.
+  * binary_tree_inorder - goes through a binary tree using in-order traversal.
  *
  * @tree: A pointer to the root node of the tree to traverse.
  * @func: A pointer to a function to call for each node.

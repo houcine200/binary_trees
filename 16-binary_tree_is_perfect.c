379261f1f95677cf946b618b17e97ff59bb69f8e
Houcine Walaq <houcine.walaq@gmai.com>
2023-11-29 16:01:47 +0100

    16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,24 +1,22 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_depth - Measure the depth of a node in a binary tree.
+ * binary_tree_nodes - Counts the nodes with at least one child
  *
- * @tree: A pointer to the node to measure the depth.
+ * @tree: Pointer to the root node of the tree to count nodes
  *
- * Return: The depth of the node in the binary tree.
+ * Return: Number of nodes with at least one child in the binary tree,
+ *         0 if tree is NULL
  */
-size_t binary_tree_depth(const binary_tree_t *tree)
+size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-	int depht_count;
-
 	if (tree == NULL)
 		return (0);
 
-	if (tree->parent == NULL)
-		return (0);
+	if (tree->left != NULL || tree->right != NULL)
+		return (binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right) + 1);
 
-	depht_count = binary_tree_depth(tree->parent) + 1;
-	return (depht_count);
+	return (0);
 }
 
 /**
@@ -39,10 +37,26 @@ int binary_tree_is_full(const binary_tree_t *tree)
 	return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
 }
 
+/**
+ * binary_tree_is_perfect - Check if a binary tree is perfect
+ * @tree: Pointer to the root node of the tree to check
+ *
+ * Return: 1 if the tree is perfect, 0 otherwise
+ */
+
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-    if (tree == NULL)
-        return 0;
+    
 
-    return binary_tree_is_full(tree) && binary_tree_depth(tree->left) == binary_tree_depth(tree->right);
+	if (tree == NULL)
+        	return 0;
+
+	if (!(binary_tree_is_full(tree)))
+		return (0);
+
+	
+	if (binary_tree_nodes(tree->left) != binary_tree_nodes(tree->right))
+		return (0);
+	return (1);
+
 }

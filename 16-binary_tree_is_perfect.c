02cb204bb3c3708bca43fbe1fbe6fa9986a8980f
Houcine Walaq <houcine.walaq@gmai.com>
2023-11-29 15:43:38 +0100

    16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -67,5 +67,5 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
     if (tree == NULL)
         return 0;
 
-    return binary_tree_is_full(tree) && binary_tree_balance(tree) == 0;
+    return binary_tree_is_full(tree) && abs(binary_tree_balance(tree)) <= 1;
 }

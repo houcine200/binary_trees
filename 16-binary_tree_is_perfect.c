c8e88ae09aa077b1cca095d4b87de9459dc8ac6c
Houcine Walaq <houcine.walaq@gmai.com>
2023-11-29 15:34:49 +0100

    16

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,72 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_height - Measure the height of a binary tree.
+ *
+ * @tree: A pointer to the root node of the tree to measure.
+ *
+ * Return: The height of the binary tree.
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	int height_left, height_right;
+
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left == NULL && tree->right == NULL)
+		return (0);
+
+	height_left = binary_tree_height(tree->left) + 1;
+	height_right = binary_tree_height(tree->right) + 1;
+
+	if (height_left > height_right)
+		return (height_left);
+	return (height_right);
+}
+/**
+ * binary_tree_balance - Measures the balance factor of a binary tree
+ *
+ * @tree: Pointer to the root node of the tree to measure balance factor
+ *
+ * Return: Balance factor of the binary tree, 0 if tree is NULL
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	int left = 0;
+	int right = 0;
+
+	if (tree == NULL)
+		return (0);
+	if (tree->left)
+		left = binary_tree_height(tree->left) + 1;
+	if (tree->right)
+		right = binary_tree_height(tree->right) + 1;
+	return (left - right);
+}
+
+/**
+ * binary_tree_is_full - Checks if a binary tree is full
+ *
+ * @tree: Pointer to the root node of the tree to check
+ *
+ * Return: 1 if the tree is full, 0 if not
+ */
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+
+	else
+		return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
+}
+
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+    if (tree == NULL)
+        return 0;
+
+    return binary_tree_is_full(tree) && binary_tree_balance(tree) == 0;
+}

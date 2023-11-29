e7f49e0bee2e23c1b07822f4cf86fd41948346f5
Houcine Walaq <houcine.walaq@gmai.com>
2023-11-29 16:42:40 +0100

    18b

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -20,11 +20,17 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 	return (NULL);
 }
 
+/**
+ * binary_tree_uncle - Find the uncle of a node in a binary tree
+ * @node: Pointer to the node to find the uncle
+ *
+ * Return: Pointer to the uncle node, or NULL if node is NULL or has no uncle
+ */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-    if (node == NULL)
-        return NULL;
-    if (binary_tree_sibling(node->parent) != NULL)
-        return binary_tree_sibling(node->parent);
-    return NULL;
+	if (node == NULL)
+		return (NULL);
+	if (binary_tree_sibling(node->parent) != NULL)
+		return (binary_tree_sibling(node->parent));
+	return (NULL);
 }

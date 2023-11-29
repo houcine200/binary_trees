f3c1d595c71653916440905a47f0cb6726e9580f
Houcine Walaq <houcine.walaq@gmai.com>
2023-11-29 14:56:32 +0100

    15

---
+++ 15-binary_tree_is_full.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+    if (tree == NULL)
+		return (0);
+	
+	if (tree->left == NULL && tree->right == NULL)
+        return 1;
+    
+    else
+        return binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right);
+}

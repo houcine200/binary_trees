385dff7d48d9c41e33d063db4fa811ccec1fbcde
Houcine Walaq <houcine.walaq@gmai.com>
2023-11-29 14:18:36 +0100

    14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -23,6 +23,13 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		return (height_left);
 	return (height_right);
 }
+/**
+ * binary_tree_balance - Measures the balance factor of a binary tree
+ *
+ * @tree: Pointer to the root node of the tree to measure balance factor
+ *
+ * Return: Balance factor of the binary tree, 0 if tree is NULL
+ */
 int binary_tree_balance(const binary_tree_t *tree)
 {
 	int left = 0;
